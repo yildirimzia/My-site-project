6d9b7fa8afe3630eea78c16534cc50ecd6fb0f1d
ziya yıldırım <ziya.yildirim@aknon.comm>
2020-05-19 20:05:04 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import IMask from 'imask'
-const css = require('./scss/style.scss')
+import  './scss/style.scss'
 import './js/contact-form';
 import music from './music/keman.mp3'
 
@@ -9,24 +9,39 @@ let auido = new Audio();
 let isPlaying = true
 auido.src = music
 
-
-
-
 window.onload = () => {
-	auido.play();
-
-
+	auido.pause();
 	play.addEventListener('click', () => {
 		if(isPlaying){
 			auido.pause()
-			console.log('true');
 			isPlaying = false
 			document.querySelector('#music-id').className = 'music-box music-not'
 		}else {
 			auido.play()
-			console.log('false');
 			isPlaying= true
 			document.querySelector('#music-id').classList.remove('music-not')
 		}
 	})
 }
+
+navigator.getBattery().then(function(battery){
+	const batteryBox = document.querySelector('.battery-content')
+	function currentBatteryInfo(){
+		const batteryte = battery.level*100
+		batteryBox.style.width = `${batteryte}%`
+
+		if(!battery.charging) {
+			if(batteryte >= 65 ) {
+				batteryBox.style.backgroundColor = 'green'
+			}else if(batteryte >= 40) {
+				batteryBox.style.backgroundColor = 'orange'
+			}else if (batteryte <= 39) {
+				batteryBox.style.backgroundColor = 'red'
+			}
+		}else {
+			batteryBox.style.backgroundColor = 'green';
+			batteryBox.innerHTML = '<ion-icon name="flash-outline"></ion-icon>'
+		}
+	}	
+  currentBatteryInfo();
+});
