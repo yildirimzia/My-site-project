94e5af3432c80eb14ddbd16faa58f2c7e0993e4d
ziya yıldırım <ziya.yildirim@aknon.comm>
2020-05-19 20:07:52 +0300

    indentation edit

--- src/index.js
+++ src/index.js
@@ -10,38 +10,38 @@ let isPlaying = true
 auido.src = music
 
 window.onload = () => {
-	auido.pause();
-	play.addEventListener('click', () => {
-		if(isPlaying){
-			auido.pause()
-			isPlaying = false
-			document.querySelector('#music-id').className = 'music-box music-not'
-		}else {
-			auido.play()
-			isPlaying= true
-			document.querySelector('#music-id').classList.remove('music-not')
-		}
-	})
+  auido.pause(); // Degistirilcek 
+  play.addEventListener('click', () => {
+    if(isPlaying){
+      auido.pause()
+      isPlaying = false
+      document.querySelector('#music-id').className = 'music-box music-not'
+    }else {
+      auido.play()
+      isPlaying= true
+      document.querySelector('#music-id').classList.remove('music-not')
+    }
+  })
 }
 
 navigator.getBattery().then(function(battery){
-	const batteryBox = document.querySelector('.battery-content')
-	function currentBatteryInfo(){
-		const batteryte = battery.level*100
-		batteryBox.style.width = `${batteryte}%`
+  const batteryBox = document.querySelector('.battery-content')
+  function currentBatteryInfo(){
+    const batteryte = battery.level*100
+    batteryBox.style.width = `${batteryte}%`
 
-		if(!battery.charging) {
-			if(batteryte >= 65 ) {
-				batteryBox.style.backgroundColor = 'green'
-			}else if(batteryte >= 40) {
-				batteryBox.style.backgroundColor = 'orange'
-			}else if (batteryte <= 39) {
-				batteryBox.style.backgroundColor = 'red'
-			}
-		}else {
-			batteryBox.style.backgroundColor = 'green';
-			batteryBox.innerHTML = '<ion-icon name="flash-outline"></ion-icon>'
-		}
-	}	
+    if(!battery.charging) {
+      if(batteryte >= 65 ) {
+        batteryBox.style.backgroundColor = 'green'
+      }else if(batteryte >= 40) {
+        batteryBox.style.backgroundColor = 'orange'
+      }else if (batteryte <= 39) {
+        batteryBox.style.backgroundColor = 'red'
+      }
+    }else {
+      batteryBox.style.backgroundColor = 'green';
+      batteryBox.innerHTML = '<ion-icon name="flash-outline"></ion-icon>'
+    }
+  }	
   currentBatteryInfo();
 });
